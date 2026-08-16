f3bda1a1f4e928c701a889e542d2c81a5e5cfae1
EVBLOOD <saad.akllam@gmail.com>
2026-08-16 21:34:09 +0100

    refactor(comments): remove CommentsGateway from CommentsModule

--- srcs/backend/src/comments/comments.module.ts
+++ srcs/backend/src/comments/comments.module.ts
@@ -3,7 +3,6 @@ import { CommentsService } from "./comments.service";
 import { CommentsController } from "./comments.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Comment } from "./entities/comment.entity";
-import { CommentsGateway } from "./comments.gateway";
 import { UsersModule } from "src/users/users.module";
 import { MoviesModule } from "src/movies/movies.module";
 import { CommentCommentInteraction } from "./entities/user-comment.entity";
@@ -11,6 +10,6 @@ import { CommentCommentInteraction } from "./entities/user-comment.entity";
 @Module({
     imports: [TypeOrmModule.forFeature([Comment, CommentCommentInteraction]), UsersModule, MoviesModule],
     controllers: [CommentsController],
-    providers: [CommentsService, CommentsGateway],
+    providers: [CommentsService],
 })
 export class CommentsModule {}
